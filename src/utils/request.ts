34bf4affae61833b969212647f9fafeefce4bef0
顾明宸 <1240235512@qq.com>
2020-12-21 17:04:05 +0800

    'add'

---
+++ src/utils/request.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 10000
+})
+
+service.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (res.code !== 200) {
+      return Promise.reject(new Error(res.message || 'Error'))
+    } else {
+      return res
+    }
+  },
+  error => {
+    console.log('err' + error)
+    return Promise.reject(error)
+  }
+)
+
+
+export default service
