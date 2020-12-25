53c9b80b975d3880e54af698734e37cb6d3cfacd
顾明宸 <1240235512@qq.com>
2020-12-25 17:31:57 +0800

    '请求工具编写'

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,12 +1,34 @@
+'use strict'
 import axios from 'axios'
+import qs from 'qs'
+
+/**
+ * code处理
+ * @param code 
+ * @param msg 
+ */
+const codeHandle = (code: number|null, msg: string|null) => {
+  switch (code) {
+    case 401:
+      console.log(msg)
+      break
+  }
+}
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
-  timeout: 10000
+  timeout: 10000,
+  headers: {
+    'Content-Type': ContentType.JSON
+  }
 })
 
 service.interceptors.request.use(
   config => {
+    if (config.data) {
+      config.headers['Content-Type'] = ContentType.FORM
+      config.data = qs.stringify(config.data)
+    }
     return config
   },
   error => {
@@ -18,10 +40,12 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   response => {
     const res = response.data
-    if (res.code !== 200) {
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
+    if (res.code === 200) {
       return res
+    } else {
+      codeHandle(res.code, res.msg)
+      return Promise.reject(new Error(res.message || 'Error'))
+      
     }
   },
   error => {
@@ -29,6 +53,5 @@ service.interceptors.response.use(
     return Promise.reject(error)
   }
 )
-
 
 export default service
---
+++ src/utils/request.type.ts
@@ -0,0 +1,5 @@
+enum ContentType {
+  JSON = 'application/json;charset=UTF-8',
+  FORM = 'application/x-www-form-urlencoded;charset=UTF-8',
+  UPLOAD = 'multipart/form-data'
+}
