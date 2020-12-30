f0664b0ea8d6821f6dd0cc7acb11f1a81265c60a
顾明宸 <1240235512@qq.com>
2020-12-30 10:29:50 +0800

    'update'

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
-/*
- * @Description:
- * @Author: gumingchen
- * @Email: 1240235512@qq.com
- * @Date: 2020-12-15 08:45:46
- * @LastEditors: gumingchen
- * @LastEditTime: 2020-12-30 10:03:13
- */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
