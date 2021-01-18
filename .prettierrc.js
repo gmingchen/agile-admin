6aad03f78dad333c45e2255d457231579ac3e15e
顾明宸 <1240235512@qq.com>
2021-01-18 11:00:56 +0800

    'prettier'

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,13 +4,14 @@
  * @Email: 1240235512@qq.com
  * @Date: 2021-01-15 16:34:03
  * @LastEditors: gumingchen
- * @LastEditTime: 2021-01-18 10:24:34
+ * @LastEditTime: 2021-01-18 10:59:37
  */
 module.exports = {
   "printWidth": 150, // 超过最大值换行
   "tabWidth": 2, // 缩进字节数
   "semi": false, // 在语句末尾打印分号
   "singleQuote": true, // 使用引号
-  "arrowParens": "avoid", // 箭头函数加上括号
+  "arrowParens": 'avoid', // 箭头函数加上括号
   "trailingComma": 'none', // 使用尾随逗号
+  "endOfLine": 'auto' // 换行符配置
 };
--- src/views/modules/other/index.vue
+++ src/views/modules/other/index.vue
@@ -4,7 +4,7 @@
  * @Email: 1240235512@qq.com
  * @Date: 2020-12-17 10:03:07
  * @LastEditors: gumingchen
- * @LastEditTime: 2021-01-18 10:35:05
+ * @LastEditTime: 2021-01-18 10:54:01
 -->
 <template>
   <div>其他布局页面</div>
