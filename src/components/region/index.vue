500db3f11dd68532049916bfa619fae1b6aa709f
GuMingChen <1240235512@qq.com>
2022-06-17 11:28:50 +0800

    区域管理完成

---
+++ src/components/region/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <el-cascader
+    ref="refCascader"
+    v-model="value"
+    :props="regionProps"
+    :show-all-levels="false" />
+</template>
+
+<script >
+import { defineComponent, reactive, ref, toRefs } from 'vue'
+
+import useModel from '@/mixins/model'
+
+import { selectListApi } from '@/api/region'
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: [String, Number, Array],
+      required: true
+    }
+  },
+  setup(props) {
+    const value = useModel(props)
+
+    const refCascader = ref()
+    const data = reactive({
+      regionProps: {
+        emitPath: false,
+        checkStrictly: true,
+        value: 'id',
+        label: `name`,
+        children: 'children',
+        lazy: true,
+        lazyLoad: (node, resolve) => {
+          if (node.level === 0) {
+            resolve([{
+              id: 0,
+              name: '一级区域',
+              level: 0,
+              leaf: false,
+              children: []
+            }])
+          } else {
+            selectListApi(node.value).then(r => {
+              if (r) {
+                resolve(r.data.map(item => {
+                  return {
+                    ...item,
+                    leaf: node.level === 2
+                  }
+                }))
+              }
+            })
+          }
+        }
+      }
+    })
+
+    /**
+     * 获取当前选中节点
+     */
+    const getCheckedNodes = () => {
+      return refCascader.value.getCheckedNodes()[0].data
+    }
+
+    return {
+      value,
+      refCascader,
+      ...toRefs(data),
+      getCheckedNodes
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/modules/data/region/components/add-edit.vue
@@ -0,0 +1,150 @@
+<template>
+  <el-dialog
+    width="500px"
+    :title="form.id ? '编辑' : '新增'"
+    v-model="visible"
+    :close-on-click-modal="false"
+    @closed="dialogClosedHandle"
+    append-to-body
+    draggable>
+    <el-form
+      v-loading="loading"
+      :model="form"
+      :rules="rules"
+      ref="refForm"
+      @keyup.enter="submit()"
+      label-position="top">
+      <el-form-item label="名称" prop="name">
+        <el-input v-model="form.name" placeholder="名称" />
+      </el-form-item>
+      <el-form-item label="编号" prop="code">
+        <el-input v-model="form.code" placeholder="编号" />
+      </el-form-item>
+      <el-form-item label="上级" prop="parent_id">
+        <Region ref="refRegion" v-model="form.parent_id" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="visible = false">取消</el-button>
+        <el-button
+          v-repeat
+          type="primary"
+          @click="submit()">确认</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+import { defineComponent, nextTick, reactive, ref, toRefs } from 'vue'
+
+import { ElMessage } from 'element-plus'
+import Region from '@/components/region'
+
+import { isInteger } from '@/utils/regular'
+
+import { infoApi, addApi, editApi } from '@/api/region'
+
+export default defineComponent({
+  emits: ['refresh'],
+  components: { Region },
+  setup(_props, { emit }) {
+    const refForm = ref()
+    const refRegion = ref()
+    const data = reactive({
+      visible: false,
+      loading: false,
+      form: {
+        id: null,
+        name: '',
+        code: '',
+        level: '',
+        parent_id: ''
+      }
+    })
+
+    const rules = reactive(function() {
+      const checkCode = (_rule, value, callback) => {
+        if (!isInteger(value)) {
+          callback(new Error('请输入整数'))
+        } else {
+          callback()
+        }
+      }
+      return {
+        name: [{ required: true, message: '请输入名称', trigger: 'blur' }],
+        code: [
+          { required: true, message: '请输入区域编码', trigger: 'blur' },
+          { validator: checkCode, trigger: 'blur' }
+        ],
+        parent_id: [{ required: true, message: '请选择上级', trigger: 'blur' }]
+      }
+    }())
+
+    const init = async (id) => {
+      data.visible = true
+      data.loading = true
+      data.form.id = id
+
+      if (id) {
+        const r = await infoApi(data.form.id)
+        if (r) {
+          data.form.id = r.data.id
+          data.form.name = r.data.name
+          data.form.code = r.data.code
+          data.form.level = r.data.level
+          data.form.parent_id = r.data.parent_id
+        }
+      }
+      nextTick(() => { data.loading = false })
+    }
+
+    /**
+     * @description: 表单验证提交
+     * @param {*}
+     * @return {*}
+     * @author: gumingchen
+     */
+    const submit = () => {
+      refForm.value.validate(async valid => {
+        if (valid) {
+          const params = {
+            ...data.form,
+            level: refRegion.value.getCheckedNodes().level + 1
+          }
+          const r = data.form.id ? await editApi(params) : await addApi(params)
+          if (r) {
+            data.visible = false
+            ElMessage({
+              message: '操作成功!',
+              type: 'success'
+            })
+            emit('refresh')
+          }
+        }
+      })
+    }
+
+    /**
+     * @description: 弹窗关闭动画结束时的回调
+     * @param {*}
+     * @return {*}
+     * @author: gumingchen
+     */
+    const dialogClosedHandle = () => {
+      refForm.value.resetFields()
+    }
+
+    return {
+      refForm,
+      refRegion,
+      ...toRefs(data),
+      rules,
+      init,
+      submit,
+      dialogClosedHandle
+    }
+  }
+})
+</script>
