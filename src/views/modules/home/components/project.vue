48e83dfb6d169350f2dcbe2a711231b1e9f7a578
GuMingChen <1240235512@qq.com>
2022-06-30 20:12:04 +0800

    更新首页

--- src/views/modules/home/components/project.vue
+++ src/views/modules/home/components/project.vue
@@ -3,10 +3,29 @@
     <div class="panel-title margin_b-10 font-size-18">项目</div>
     <div class="panel-content">
       <div class="text font-size-12">{{ text }}</div>
-      <el-image :src="githubStars" class="margin_t-10 margin_r-5 cursor-pointer" @click="githubHandle()"  />
-      <el-image :src="githubForks" class="margin_t-10 margin_r-5 cursor-pointer" @click="githubHandle()" />
-      <el-image :src="giteeStars" class="margin_t-10 margin_r-5 cursor-pointer" @click="giteeHandle()" />
-      <el-image :src="giteeForks" class="margin_t-10 cursor-pointer" @click="giteeHandle()" />
+      <div class="margin_t-10">
+        <el-image :src="githubStars" class="margin_r-5 cursor-pointer" @click="githubHandle()"  />
+        <el-image :src="githubForks" class="margin_r-5 cursor-pointer" @click="githubHandle()" />
+        <el-image :src="giteeStars" class="margin_r-5 cursor-pointer" @click="giteeHandle()" />
+        <el-image :src="giteeForks" class="cursor-pointer" @click="giteeHandle()" />
+      </div>
+      <div class="margin_t-10">
+        <el-button
+          size="small"
+          type="success"
+          @click="releaseHandle()"
+          round>更新日志</el-button>
+        <el-button
+          size="small"
+          type="danger"
+          @click="releaseHandle()"
+          round>GitHub</el-button>
+        <el-button
+          size="small"
+          type="danger"
+          @click="releaseHandle()"
+          round>Gitee</el-button>
+      </div>
     </div>
   </div>
 </template>
@@ -23,6 +42,7 @@ export default defineComponent({
       gitee: 'https://gitee.com/shychen/vue3-element-plus-admin',
       giteeStars: 'http://gitee.com/shychen/vue3-element-plus-admin/badge/star.svg?theme=dark',
       giteeForks: 'http://gitee.com/shychen/vue3-element-plus-admin/badge/fork.svg?theme=dark',
+      release: 'https://github.com/gmingchen/vue3-element-plus-admin/releases/tag/v5.0.1',
       text: '前端基于 Vue3、Element-plus、Vue-router、Vux、Sass、Axios 等技术栈。后端基于 Java 的 Springboot、Mysql、Shiro、Mybatis-plus 等技术栈。如果喜欢就点个星星支持一下。'
     })
 
@@ -33,10 +53,15 @@ export default defineComponent({
     const giteeHandle = () => {
       window.open(data.gitee)
     }
+
+    const releaseHandle = () => {
+      window.open(data.release)
+    }
     return {
       ...toRefs(data),
       githubHandle,
-      giteeHandle
+      giteeHandle,
+      releaseHandle
     }
   }
 })
