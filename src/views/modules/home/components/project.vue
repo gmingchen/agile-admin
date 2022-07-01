7ec639890f232ac193637db7827158564132cbcb
GuMingChen <1240235512@qq.com>
2022-07-01 12:46:40 +0800

    update

--- src/views/modules/home/components/project.vue
+++ src/views/modules/home/components/project.vue
@@ -8,6 +8,7 @@
         <el-image :src="githubForks" class="margin_r-5 cursor-pointer" @click="githubHandle()" />
         <el-image :src="giteeStars" class="margin_r-5 cursor-pointer" @click="giteeHandle()" />
         <el-image :src="giteeForks" class="cursor-pointer" @click="giteeHandle()" />
+        <el-image :src="oscsImage" class="cursor-pointer" @click="oscsHandle()" />
       </div>
       <div class="margin_t-10">
         <el-button
@@ -43,6 +44,8 @@ export default defineComponent({
       giteeStars: 'http://gitee.com/shychen/vue3-element-plus-admin/badge/star.svg?theme=dark',
       giteeForks: 'http://gitee.com/shychen/vue3-element-plus-admin/badge/fork.svg?theme=dark',
       release: 'https://github.com/gmingchen/vue3-element-plus-admin/releases/tag/v5.0.1',
+      oscs: 'https://www.oscs1024.com/project/gmingchen/vue3-element-plus-admin?ref=badge_small',
+      oscsImage: 'https://www.oscs1024.com/platform/badge/gmingchen/vue3-element-plus-admin.svg?size=small',
       text: '前端基于 Vue3、Element-plus、Vue-router、Vux、Sass、Axios 等技术栈。后端基于 Java 的 Springboot、Mysql、Shiro、Mybatis-plus 等技术栈。如果喜欢就点个星星支持一下。'
     })
 
@@ -57,11 +60,16 @@ export default defineComponent({
     const releaseHandle = () => {
       window.open(data.release)
     }
+
+    const oscsHandle = () => {
+      window.open(data.release)
+    }
     return {
       ...toRefs(data),
       githubHandle,
       giteeHandle,
-      releaseHandle
+      releaseHandle,
+      oscsHandle
     }
   }
 })
