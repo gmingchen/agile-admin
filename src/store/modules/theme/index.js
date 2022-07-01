81e5218e88f62bfea6692eed252508619d41619c
GuMingChen <1240235512@qq.com>
2022-07-01 13:35:46 +0800

    update

--- src/store/modules/theme/index.js
+++ src/store/modules/theme/index.js
@@ -80,7 +80,6 @@ export default {
         for (const key in defaultTheme.text) {
           result.text[key] = getComputedStyle(el).getPropertyValue(`--el-text-color-${ key }`)
         }
-        console.log(123)
         result.menu.backgroundColor = getComputedStyle(el).getPropertyValue(`--gl-sidebar-background-color`)
         // result.menu.textColor = getComputedStyle(el).getPropertyValue(`--el-text-color-primary`)
         result.menu.activeTextColor = getComputedStyle(el).getPropertyValue(`--el-color-primary`)
