0da1d66460db70964538cac73b631d50610b3010
gumingchen <1240235512@qq.com>
2022-10-31 16:48:27 +0800

    update

--- src/views/modules/system/menu/components/edit.vue
+++ src/views/modules/system/menu/components/edit.vue
@@ -50,7 +50,7 @@
 import { reactive, ref } from 'vue'
 
 import { ElMessage } from 'element-plus'
-import IconSelectInput from '@/components/icon-select-input'
+import IconSelectInput from '@/components/icon-select-input/index.vue'
 
 import { updateApi } from '@/api/enterprise-menu'
 
