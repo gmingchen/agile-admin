bd196e9a80242019f4ce7304afa8ba63996f3e6b
gumingchen <1240235512@qq.com>
2025-09-17 13:11:37 +0800

    用户管理调整

--- src/permission/modules/user/index.js
+++ src/permission/modules/user/index.js
@@ -3,7 +3,7 @@ import { PERMISSION_TYPE_ENUM } from '@/common/enums'
 export default [
   {
     label: '用户管理',
-    value: 'user',
+    value: 168,
     icon: 'user',
     permission: 'catalog:user',
     show: true,
@@ -11,7 +11,7 @@ export default [
     children: [
       {
         label: '用户列表',
-        value: 'user-list',
+        value: 169,
         icon: 'user',
         permission: 'user:page',
         keepalive: false,
@@ -25,15 +25,15 @@ export default [
           component: '/user/list/index',
         },
         children: [
-          { label: '查看', value: 'user-view', permission: 'user:info', type: PERMISSION_TYPE_ENUM.BUTTON, },
-          { label: '新增', value: 'user-create', permission: 'user:create', type: PERMISSION_TYPE_ENUM.BUTTON, },
-          { label: '编辑', value: 'user-update', permission: 'user:update,user:info', type: PERMISSION_TYPE_ENUM.BUTTON, },
-          { label: '删除', value: 'user-delete', permission: 'user:delete', type: PERMISSION_TYPE_ENUM.BUTTON, },
+          { label: '查看', value: 170, permission: 'user:info', type: PERMISSION_TYPE_ENUM.BUTTON, },
+          { label: '删除', value: 171, permission: 'user:delete', type: PERMISSION_TYPE_ENUM.BUTTON, },
+          { label: '设置是否禁用', value: 172, permission: 'user:status', type: PERMISSION_TYPE_ENUM.BUTTON, },
+          { label: '导出', value: 173, permission: 'user:export', type: PERMISSION_TYPE_ENUM.BUTTON, },
         ]
       },
       {
         label: '用户详情',
-        value: 'user-details',
+        value: 174,
         icon: '',
         permission: 'user:info',
         keepalive: false,
